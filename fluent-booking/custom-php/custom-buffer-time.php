105fb4b55155ab8edc7ad1947c5f9b3c127d265a
rakidhasan93 <rakid.authlab@gmail.com>
2025-11-18 21:45:24 +0600

    Delete fluent-booking/custom-php directory

--- fluent-booking/custom-php/custom-buffer-time.php
+++
@@ -1,8 +0,0 @@
-<?php
-add_filter('fluent_booking/buffer_times_schema', function ($times) {
-    $times[] = [
-        'value' => '40',
-        'label' => __('40 Minutes', 'fluent-booking')
-    ];
-    return $times;
-}); 
--- fluent-booking/custom-php/custom-timeslot-interval.php
+++
@@ -1,7 +0,0 @@
-add_filter('fluent_booking/slot_intervals_schema', function ($intervals) {
-    $intervals[] = [
-        'value' => '40',
-        'label' => __('40 Minutes', 'fluent-booking')
-    ];
-    return $intervals;
-}); 
