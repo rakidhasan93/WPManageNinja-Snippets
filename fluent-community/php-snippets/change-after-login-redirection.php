4d7a6293946fe81a6143c15ea6e27f8b3386757b
rakidhasan93 <rakid.authlab@gmail.com>
2025-11-18 23:30:18 +0600

    Remove login redirection for community portal
    Remove the login redirection functionality for the community portal.

--- fluent-community/php-snippets/change-after-login-redirection.php
+++ fluent-community/php-snippets/change-after-login-redirection.php
@@ -1,12 +1,1 @@
-add_action('template_redirect', function () {
-    $portal_slug = 'community';
 
-    // Current request path without domain/query
-    $req = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
-
-    // Only redirect if visiting exactly /community AND user is logged in
-    if ($req === $portal_slug && is_user_logged_in()) {
-        wp_safe_redirect(home_url("/{$portal_slug}/courses"), 302);
-        exit;
-    }
-}, 0);
