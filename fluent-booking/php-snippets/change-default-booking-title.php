f2f8bcf3a554fe1b8b1b8d59c7703b85dca9931a
Md Rakid Hasan Chowdhury <rakid.authlab@gmail.com>
2025-11-21 18:00:35 +0600

    Update change-default-booking-title.php

--- fluent-booking/php-snippets/change-default-booking-title.php
+++ fluent-booking/php-snippets/change-default-booking-title.php
@@ -1,4 +1,4 @@
 add_filter('fluent_booking/booking_meeting_title', function($meetingTitle, $authorName, $guestName, $calendarSlot, $instance) {
-    $meetingTitle = "$calendarSlot->description Appointment Between {$authorName} and {$guestName}";
+    $meetingTitle = "Appointment Between {$authorName} and {$guestName}";
     return $meetingTitle;
 }, 10, 5);
