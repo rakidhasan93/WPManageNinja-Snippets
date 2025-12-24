776fa6385ea3ad613a64b74b0bd8fdd307dca789
Md Rakid Hasan Chowdhury <rakid.authlab@gmail.com>
2025-12-24 10:00:44 +0600

    Update change-email-field-label.php

--- fluent-community/php-snippets/change-email-field-label.php
+++ fluent-community/php-snippets/change-email-field-label.php
@@ -1,24 +1,17 @@
 add_filter('gettext', function ($translated, $text, $domain) {
 
-    // Scope strictly to FluentCommunity login form
-    if (
-        $domain !== 'fluent-community' ||
-        empty($_GET['fcom_action']) ||
-        $_GET['fcom_action'] !== 'auth' ||
-        empty($_GET['form']) ||
-        $_GET['form'] !== 'login'
-    ) {
+    if ($domain !== 'fluent-community') {
         return $translated;
     }
 
-    if ($text === 'Email Address') {
-        return 'Username';
-    }
+    switch ($text) {
+        case 'Email Address':
+            return 'Username';
 
-    if ($text === 'Your account email address') {
-        return 'Enter your username';
+        case 'Your account email address':
+            return 'Enter your username';
     }
 
     return $translated;
 
-}, 20, 3);
+}, 10, 3);
