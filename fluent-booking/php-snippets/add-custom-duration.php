2ab80283273cc8b6137f7c72c50054c5a0d7774c
Md Rakid Hasan Chowdhury <rakid.authlab@gmail.com>
2026-01-14 23:04:22 +0600

    Update meeting duration options
    Removed 300 and 600 minutes options from meeting durations.

--- fluent-booking/php-snippets/add-custom-duration.php
+++ fluent-booking/php-snippets/add-custom-duration.php
@@ -1,12 +1,9 @@
 add_filter('fluent_booking/meeting_multi_durations_schema', function ($durations) {
     $durations[] = [
-        'value' => '300',
-        'label' => __('300 Minutes', 'fluent-booking') // 5 hours
+        'value' => '20',
+        'label' => __('20 Minutes', 'fluent-booking') // 5 hours
     ];
 
-    $durations[] = [
-        'value' => '600',
-        'label' => __('600 Minutes', 'fluent-booking') // 6 hours
-    ];
 return $durations;
+
 });
