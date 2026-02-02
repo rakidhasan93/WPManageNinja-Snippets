1003a58f30b84683bf2b19bcfd65f37b51ab0120
Md Rakid Hasan Chowdhury <rakid.authlab@gmail.com>
2026-02-03 00:53:19 +0600

    Update reschedule settings for booking restrictions

--- fluent-booking/php-snippets/reschedule-time-limit.php
+++ fluent-booking/php-snippets/reschedule-time-limit.php
@@ -1,9 +1,9 @@
 add_filter('fluent_booking/get_calendar_event_settings', function($settings) {
-    // Override the default cancellation settings
-    $settings['can_not_cancel'] = [
+    // Override the default reschdule settings
+    $settings['can_not_reschedule'] = [
         'enabled'   => true,
-        'message'   => 'Sorry! you can not cancel this booking within 48 hours of the meeting',
-        'type'      => 'always', // This prevents ALL cancellations
+        'message'   => 'Sorry! you can not reschedule this booking within 48 hours of the meeting',
+        'type'      => 'always', // This prevents ALL reschedule
         'condition' => [
             'unit'  => 'hours',
             'value' => 48
