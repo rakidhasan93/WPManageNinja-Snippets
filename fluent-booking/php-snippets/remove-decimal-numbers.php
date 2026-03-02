35b483de6a29d7a4a8715503ca20b181a4727691
Md Rakid Hasan Chowdhury <rakid.authlab@gmail.com>
2026-03-03 04:21:01 +0600

    Set decimal points to 0 in currency settings

--- fluent-booking/php-snippets/remove-decimal-numbers.php
+++ fluent-booking/php-snippets/remove-decimal-numbers.php
@@ -1,4 +1,18 @@
+<?php
+    
+// Change default decimal points to 0
+
 add_filter('fluent_booking/default_currency_settings', function($defaults) {
-    $defaults['decimal_points'] = 0;
+    $defaults['decimal_points'] = 0;    
     return $defaults;
 });
+
+    
+// For example: 100 $ (symbol on the right with space)
+
+add_filter('fluent_booking/global_currency_settings', function ($settings) {
+    $settings['currency_position'] = 'right_space'; // or 'left_space'
+    return $settings;
+});
+
+?>
