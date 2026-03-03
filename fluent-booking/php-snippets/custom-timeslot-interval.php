b624e501325099869e86e1d1a23e8d44e2a6d1ea
Md Rakid Hasan Chowdhury <rakid.authlab@gmail.com>
2026-03-04 01:55:49 +0600

    Change timeslot interval to 600 minutes

--- fluent-booking/php-snippets/custom-timeslot-interval.php
+++ fluent-booking/php-snippets/custom-timeslot-interval.php
@@ -1,7 +1,7 @@
 add_filter('fluent_booking/slot_intervals_schema', function ($intervals) {
     $intervals[] = [
-        'value' => '40',
-        'label' => __('40 Minutes', 'fluent-booking')
+        'value' => '600',
+        'label' => __('600 Minutes', 'fluent-booking')
     ];
     return $intervals;
 }); 
