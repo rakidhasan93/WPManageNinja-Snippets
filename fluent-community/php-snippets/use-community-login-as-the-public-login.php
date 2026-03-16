e4afa3db2dec73070640532ca71655128f10ab84
Md Rakid Hasan Chowdhury <rakid.authlab@gmail.com>
2026-03-17 01:07:36 +0600

    Add community login as public login URL

---
+++ fluent-community/php-snippets/use-community-login-as-the-public-login.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Use /community-login as the public login URL.
+ */
+
+add_action('init', function () {
+    add_rewrite_rule('^community-login/?$', 'wp-login.php', 'top');
+});
+
+add_filter('login_url', function ($login_url, $redirect, $force_reauth) {
+    $url = home_url('/community-login');
+    if (!empty($redirect)) {
+        $url = add_query_arg('redirect_to', rawurlencode($redirect), $url);
+    }
+    return $url;
+}, 10, 3);
+
+add_action('login_init', function () {
+    if ($_SERVER['REQUEST_METHOD'] === 'GET' && strpos($_SERVER['REQUEST_URI'], 'wp-login.php') !== false) {
+        wp_safe_redirect(home_url('/community-login'));
+        exit;
+    }
+});
