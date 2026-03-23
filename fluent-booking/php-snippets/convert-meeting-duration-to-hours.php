ed19241669a1720321f586191f74bc77fae9b6d7
Md Rakid Hasan Chowdhury <rakid.authlab@gmail.com>
2026-03-23 21:07:28 +0600

    Refactor meeting duration label formatting

--- fluent-booking/php-snippets/convert-meeting-duration-to-hours.php
+++ fluent-booking/php-snippets/convert-meeting-duration-to-hours.php
@@ -1,10 +1,32 @@
+<?php
+
 add_filter('fluent_booking/meeting_multi_durations_schema', function ($durations) {
     foreach ($durations as &$duration) {
         $minutes = (int) $duration['value'];
+
         if ($minutes >= 60) {
-            $hours = $minutes / 60;
-            $duration['label'] = sprintf(__('%d Hour%s', 'fluent-booking'), $hours, $hours > 1 ? 's' : '');
+            $hours = intdiv($minutes, 60);
+            $remaining_minutes = $minutes % 60;
+
+            $parts = [];
+
+            if ($hours > 0) {
+                $parts[] = sprintf(
+                    _n('%d hour', '%d hours', $hours, 'fluent-booking'),
+                    $hours
+                );
+            }
+
+            if ($remaining_minutes > 0) {
+                $parts[] = sprintf(
+                    _n('%d minute', '%d minutes', $remaining_minutes, 'fluent-booking'),
+                    $remaining_minutes
+                );
+            }
+
+            $duration['label'] = implode(' ', $parts);
         }
     }
+
     return $durations;
 });
