9043fbe40d2acd2d9a92a39489c6bc15a7e09d18
Md Rakid Hasan Chowdhury <rakid.authlab@gmail.com>
2026-04-17 00:37:48 +0600

    Add custom meeting durations to booking schema
    Added custom meeting durations of 20 and 40 minutes to the booking schema.

--- fluent-booking/php-snippets/add-custom-duration.php
+++ fluent-booking/php-snippets/add-custom-duration.php
@@ -1,9 +1,45 @@
+<?php
+
+add_filter('fluent_booking/meeting_durations_schema', function ($durations) {
+    $exists = wp_list_pluck($durations, 'value');
+
+    if (!in_array('20', $exists, true)) {
+        array_splice($durations, 1, 0, [[
+            'value' => '20',
+            'label' => __('20 Minutes', 'fluent-booking')
+        ]]);
+    }
+
+    $exists = wp_list_pluck($durations, 'value');
+
+    if (!in_array('40', $exists, true)) {
+        array_splice($durations, 3, 0, [[
+            'value' => '40',
+            'label' => __('40 Minutes', 'fluent-booking')
+        ]]);
+    }
+
+    return $durations;
+});
+
 add_filter('fluent_booking/meeting_multi_durations_schema', function ($durations) {
-    $durations[] = [
-        'value' => '20',
-        'label' => __('20 Minutes', 'fluent-booking') 
-    ];
+    $exists = wp_list_pluck($durations, 'value');
 
-return $durations;
+    if (!in_array('20', $exists, true)) {
+        array_splice($durations, 3, 0, [[
+            'value' => '20',
+            'label' => __('20 Minutes', 'fluent-booking')
+        ]]);
+    }
 
+    $exists = wp_list_pluck($durations, 'value');
+
+    if (!in_array('40', $exists, true)) {
+        array_splice($durations, 5, 0, [[
+            'value' => '40',
+            'label' => __('40 Minutes', 'fluent-booking')
+        ]]);
+    }
+
+    return $durations;
 });
