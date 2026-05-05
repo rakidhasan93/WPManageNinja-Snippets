9ff2fb2a25130a53cce3ded50a784aa74a9e5ba5
Md Rakid Hasan Chowdhury <rakid.authlab@gmail.com>
2026-05-06 05:50:23 +0600

    Change public login URL from '/community-login' to '/community'

--- fluent-community/php-snippets/use-community-login-as-the-public-login.php
+++ fluent-community/php-snippets/use-community-login-as-the-public-login.php
@@ -5,11 +5,11 @@
  */
 
 add_action('init', function () {
-    add_rewrite_rule('^community-login/?$', 'wp-login.php', 'top');
+    add_rewrite_rule('^community', 'wp-login.php', 'top');
 });
 
 add_filter('login_url', function ($login_url, $redirect, $force_reauth) {
-    $url = home_url('/community-login');
+    $url = home_url('/community');
     if (!empty($redirect)) {
         $url = add_query_arg('redirect_to', rawurlencode($redirect), $url);
     }
@@ -18,7 +18,7 @@ add_filter('login_url', function ($login_url, $redirect, $force_reauth) {
 
 add_action('login_init', function () {
     if ($_SERVER['REQUEST_METHOD'] === 'GET' && strpos($_SERVER['REQUEST_URI'], 'wp-login.php') !== false) {
-        wp_safe_redirect(home_url('/community-login'));
+        wp_safe_redirect(home_url('/community'));
         exit;
     }
 });
