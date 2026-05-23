ee4176e7297ca4f72903b394d8b5f4702fb46bc4
Md Rakid Hasan Chowdhury <rakid.authlab@gmail.com>
2026-05-23 17:02:47 +0600

    Add date and time formatting to confirmation page

---
+++ fluent-booking/php-snippets/update-date-time-format-in-confirmation-page.php
@@ -0,0 +1,24 @@
+<?php
+
+add_filter('fluent_booking/schedule_receipt_data', function ($data, $booking) {
+    if (empty($data['sections']['when'])) {
+        return $data;
+    }
+
+    $timezone = $booking->person_time_zone ?: 'UTC';
+
+    try {
+        $date = new DateTime($booking->start_time, new DateTimeZone('UTC'));
+        $date->setTimezone(new DateTimeZone($timezone));
+
+        $formatted = wp_date('D d M Y H:i', $date->getTimestamp(), new DateTimeZone($timezone));
+
+        $data['sections']['when']['content'] = $formatted . ' (' . $timezone . ')';
+    } catch (Exception $e) {
+        // Keep original output if anything fails.
+    }
+
+    return $data;
+}, 10, 2);
+
+?>
