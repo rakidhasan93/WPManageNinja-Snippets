7d590feb33ab7a06ab3db36d1ecce289d7b6aa61
Md Rakid Hasan Chowdhury <rakid.authlab@gmail.com>
2026-06-25 16:41:39 +0600

    Add persistent login cookie for magic login users

---
+++ fluent-security/php-snippets/force-persistent-login-cookie-for-every-magic-login.php
@@ -0,0 +1,12 @@
+add_action('wp_login', function ($user_login, $user) {
+    if (
+        !class_exists('\FluentAuth\App\Helpers\Helper') ||
+        \FluentAuth\App\Helpers\Helper::getLoginMedia() !== 'magic_login'
+    ) {
+        return;
+    }
+
+    if ($user instanceof WP_User) {
+        wp_set_auth_cookie($user->ID, true, is_ssl());
+    }
+}, 99, 2);
